881c03b91ee7b4158141211d21e3f94d5a040e28
fadi <fadi@colabo.com>
2020-08-24 01:19:39 +0300

    added extra task

--- first_assignment.py
+++ first_assignment.py
@@ -45,8 +45,33 @@ def challenge():
     print(str(a) + b)
 
 
+def extra1():
+    var1 = 5
+    var2 = "5"
+    res = var1 == var2
+    sum = var1 + int(var2)
+    print("total sum is larger than 5.5") if sum > 5.5 else print("total sum is less than 5.5")
+    if len("hello") > len("helloo"):
+        return True
+
+
+def extra2():
+    while True:
+        name = input("Please enter your name: ")
+        if name[0] != 'A':
+            print("Sorry, your provided name does not start with A character.")
+            continue
+        else:
+            break
+    age = input("Please enter your age: ")
+    if not isinstance(age, int):
+        print("age input has which has been entered is not an integer")
+    work = input("Please enter your work field: ")
+    print("your stored work field is %s" % work)
+
+
 def main():
-    task_a()
+    extra2()
 
 
 if __name__ == "__main__":
