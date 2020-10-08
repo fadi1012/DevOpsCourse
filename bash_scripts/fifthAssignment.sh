fae858f924cb54bcb2d2a0c815d23e504cdfd1a5
fadi <fadi@colabo.com>
2020-10-08 13:15:10 +0300

    create selenium tests jenkins file

--- bash_scripts/fifthAssignment.sh
+++ bash_scripts/fifthAssignment.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
 echo $NAME
+echo "creating file"
 touch myFile.txt && chmod 775 myFile.txt
+echo "writing content into newly created file"
 echo $NAME > myFile.txt
 
 # part 2
+echo "printing newly created file content"
 cat myFile.txt
 
 # part 3
-
+echo "printing out disk free space"
 df -h
 
 # part 4
+echo "moving file to world_of_games dir"
 mv myFile.txt world_of_games/
